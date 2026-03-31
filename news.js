d9de53b634b77469dfc17e6abcfdf0f08b5a7678
Md Aktarul Islam <112043006+aktarul1973@users.noreply.github.com>
2026-03-31 06:03:35 +0600

    Update news.js to include new feed and reset to previous version

--- news.js
+++ news.js
@@ -1,7 +1,17 @@
 // Updated code in news.js to show headlines as feeds load
 
 function loadFeeds() {
-    const feeds = [/* array of feeds */];
+    const feeds = [
+        "https://www.bbc.com/bengali/index.xml",
+        "https://www.arthosuchak.com/feed/",
+        "https://www.amardesh.com/feed/",
+        "https://www.karatoa.com.bd/feed/",
+        "https://www.bdnews24.com/rss/bangla",
+        "https://feeds.bbci.co.uk/news/rss.xml",
+        "https://www.prothomalo.com/feed",
+        "https://www.kalerkantho.com/rss.xml",
+        "https://www.timesofisrael.com/feed/"
+    ];
     feeds.forEach(feed => {
         fetch(feed.url)
             .then(response => response.json())
